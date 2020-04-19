99ff69954a552dee4b33803caad1fd65079b6ade
Javier Novella <fnovella@gmail.com>
2020-04-19 13:01:27 +0200

    Basic Admin model + interface

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   root to: "home#index"
 
   resources :publications, only: [:index, :show]
+
+  namespace :admin do
+    resources :publications
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Users.create(
+  email: 'fnovella@example.com',
+  password: '123456',
+  is_admin: true
+)
